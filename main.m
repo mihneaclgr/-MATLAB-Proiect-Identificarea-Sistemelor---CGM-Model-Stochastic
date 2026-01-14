996121a1e1155e5334454ec94b8d16c146a8a263
mihneaclgr <mihneaclgr@gmail.com>
2026-01-14 16:22:51 +0200

    Varianta finala

--- main.m
+++ main.m
@@ -1,22 +1,20 @@
-T = readtable("G:\Facultate\MATLAB\Proiect IS\HUPA-UCM Diabetes Dataset\Preprocessed\HUPA0001P.csv");
+T = readtable("G:\Facultate\MATLAB\Proiect IS\HUPA-UCM Diabetes Dataset\Preprocessed\HUPA0004P.csv");
 Ts = 300;
-T.glucose = smoothdata(T.glucose, 'movmean', 5); 
 
 data = iddata(T.glucose, [T.heart_rate T.basal_rate T.calories T.steps], Ts);
-data = detrend(data);
+data = detrend(data,0);
+T.glucose = smoothdata(T.glucose, 'movmean', 5); 
 
 [~,l1] = CorCalculator(T.heart_rate, T.glucose);
 [~,l2] = CorCalculator(T.basal_rate, T.glucose);
 [~,l3] = CorCalculator(T.calories, T.glucose);
 [~,l4] = CorCalculator(T.steps, T.glucose);
 
-% Creștem ordinele nb și nf semnificativ pentru u3 și u4 (pozițiile 3 și 4)
-% pentru a forța modelul să capteze dinamica acestora care "scăpa" în reziduuri.
 nk = [l1 l2 l3 l4]; 
-nb = [4 4 4 4];   
-nf = [4 4 4 4];   
-nc = 6; 
-nd = 6; 
+nb = [2 2 2 2];   
+nf = [3 3 3 3];   
+nc = 2; 
+nd = 2; 
 
 idx = floor(0.7 * height(T));
 data_id  = data(1:idx);
@@ -24,7 +22,7 @@ data_val = data(idx+1:end);
 
 opt = bjOptions;
 opt.Focus = 'prediction';
-opt.Regularization.Lambda = 0.01; % Lambda foarte mic pentru a lăsa modelul să "urmărească" datele mai strâns
+opt.Regularization.Lambda = 0.01; 
 opt.SearchOptions.MaxIterations = 100;
 
 model_bj = bj(data_id, [nb nc nd nf nk], opt);
